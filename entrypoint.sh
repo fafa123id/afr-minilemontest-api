ad1a3bb044691fe20b2372a2c83ae79ae9fdb280
fafa123id <104996246+fafa123id@users.noreply.github.com>
2025-08-14 19:00:35 +0700

    fix: ensure .env file is created if missing and enforce key generation in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,22 +9,21 @@ do
 done
 echo "Database is ready!"
 
-if [ ! -f ".env" ]; then
-    cp .env.example .env
-fi
+php artisan migrate --force
 
-php artisan key:generate
+php artisan key:generate --force
 php artisan config:clear
 php artisan cache:clear
 php artisan view:clear
-php artisan migrate --force
 
-echo "Generating Swagger documentation..."
+
+
 php artisan l5-swagger:generate
 
 chown -R www-data:www-data storage bootstrap/cache
 chmod -R 775 storage bootstrap/cache
 
-echo "Laravel setup is complete. Starting PHP-FPM..."
+composer dump-autoload -o
 
+echo "Laravel setup is complete. Starting PHP-FPM..."
 exec "$@"
